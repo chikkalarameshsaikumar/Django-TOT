dd974481c6c42409bcb46b8d8337cbdcb12bd62c
NRK <rravikumar34@gmail.com>
2020-12-01 15:42:27 +0530

    Django Day-2 Updated

---
+++ Day-1/Ownpak/sample.py
@@ -0,0 +1,3 @@
+def mul(a,b):
+    '''To print the mulriplication of 2 numbers'''
+    return a*b
---
+++ Day-1/Ownpak/sampleoop.py
@@ -0,0 +1,13 @@
+class MyMath():
+    '''This is the documentation from packages and modules by using oops'''
+    
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+        
+    def exp(self):
+        return self.a ** self.b
+    
+    def add(self):
+        return self.a + self.b
+    
